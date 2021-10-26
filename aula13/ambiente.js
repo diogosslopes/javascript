1264ac5a63b8f0e65c27803fd8e6d856f70321ed
diogosslopes <diogodil@hotmail.com>
2021-10-26 15:36:15 -0300

    teste

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+var cont = 0
+do {
+    console.log('Tudo bem ?')
+    cont++
+}while(cont <6)
+console.log(`Fim ! O programa foi executado ${cont - 1} vezes` )
+
+/*var cont = 0
+while(cont <6){
+    console.log('Tudo bem ?')
+    cont++
+}
+
+console.log(`Fim ! O programa foi executado ${cont - 1} vezes` )*/
