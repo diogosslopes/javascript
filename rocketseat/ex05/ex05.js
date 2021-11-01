afb3818bd9bcd39213965a6eec3218a2d89662b3
diogosslopes <diogodil@hotmail.com>
2021-11-01 17:00:57 -0300

    subindo ex de js

---
+++ rocketseat/ex05/ex05.js
@@ -0,0 +1,51 @@
+//1. Declarar variavel de nome weight
+let weight
+
+//2. Que tipo de dado é a variável acima?
+
+console.log(typeof(weight))
+
+//3 Declarar variáveis.
+
+//let name = 'Diogo'
+//let age = 34
+//let weight = 60.1
+//let isSubscribed = true
+
+console.log(typeof(stars))
+
+// 4 Declarar um object com os dados acima.
+
+let student = {
+    name: "Diogo",
+    age: 34,
+    weight: 8.9,
+    isSubscribed: true
+}
+
+console.log(student)
+
+// imprimindo os dados do objeto na tela
+console.log(`${student.name} de idade ${student.age} pesa ${weight}`)
+
+//criando array students
+let students = []
+
+//adicionando o objeto student no array students
+students.push(student)
+console.log(students[0])
+
+let pamela = {
+    name: 'Pâmela',
+    age: 30,
+    weight: 55,
+    isSubscribed: true
+}
+
+students[1] = pamela
+
+console.log(students[1])
+
+
+console.log(a)
+var a = 1
---
+++ rocketseat/ex06/ex06.js
@@ -0,0 +1,11 @@
+//Declação de funções
+
+function createPhrases(){
+    console.log('Estudar é muito bom !')
+    console.log('Paciência e persistência')
+    console.log('Revisão é mãe do aprendizado')
+}
+
+//chamar a função
+
+createPhrases()
